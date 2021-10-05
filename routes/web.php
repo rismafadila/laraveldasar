c48c27466960ed0e867f640306ad137b1e9ab341
rismafadila <rismaf651@gmail.com>
2021-10-05 10:39:47 +0700

    membuat route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hallo';
 });
+
+Route::get('/home', function () {
+    return 'halaman home';
+});
+
+Route::get('/beranda', function () {
+    return 'halaman beranda';
+});
+
+Route::get('/about', function () {
+    return 'halaman about';
+});
+
+Route::get('/laravel', function () {
+    return 'halaman laravel';
+});
+
+Route::get('/nama-framework/{nama}/{tahun}', function ($nama,$tahun) {
+    return 'nama framework yang dimasukan adalah :' .$nama. '<br> tahun <b>rilis</b> :' .$tahun;
+});
+//optional parameter
+Route::get('/nama-saya-adalah/{nama?}', function ($nama = 'wajib diisi') {
+    return 'nama saya adalah :'.$nama;
+});
+
+Route::get('/hitung-luas-segitiga/{alas}/{tinggi}', function ($alas,$tinggi) {
+    return 'hasil luas segitiga :' .($alas * $tinggi) * 0.5 ;
+});
+
+
+
